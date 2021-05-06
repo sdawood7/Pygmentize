bc37e60027ee2ab7d173009f47f2385291f782a7
trident0011 <35812634+trident0011@users.noreply.github.com>
2021-05-05 21:56:22 -0400

    Read image.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
+import os
 import argparse
+import cv2 as cv
+import numpy as np
 
 parser = argparse.ArgumentParser()
 
@@ -6,4 +9,17 @@ parser.add_argument("--img_path", type=str)
 
 flags = parser.parse_args()
 
+def read_img(path):
+    if not os.path.exists(path):
+        print("Could not find given path. Please enter a valid path to the image.")
+        return None
+
+    with open(path, "r") as f:
+        temp_img = cv.imread(path)
+        img = np.asarray(temp_img)
+        return img
+
 print('Welcome to Pygmentize!')
+bwImage = read_img(flags.img_path)
+if bwImage.any():
+    print("Success!")
