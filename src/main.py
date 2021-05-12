91e3386b65a146acd8f2cbcac0dde449c3d74803
unknown <trident0011@gmail.com>
2021-05-11 21:03:52 -0400

    Add bw to color support.

--- src/main.py
+++ src/main.py
@@ -13,15 +13,15 @@ green_c2b_ratio = 0.3334
 
 #constants for converting from bw to color
 #sum of ratios must be <= 3.0
-red_b2c_ratio = 0.3333
-blue_b2c_ratio = 0.3333
-green_b2c_ratio = 0.3334
+red_b2c_ratio = 1
+blue_b2c_ratio = 0
+green_b2c_ratio = 1
 
 #this object is used to parse command line arguments :D
 parser = argparse.ArgumentParser()
 #this is an argument for the directory of an img
 parser.add_argument("--img_path", type=str, help="The path to the input image.")
-parser.add_argument("--out_dir", type=str, default=".\out_dir\\", help="The directory path to output new images to.")
+parser.add_argument("--out_path", type=str, default=".\out_dir\\", help="The directory path to output new images to.")
 #this stores the values of the parsed command line arguments
 flags = parser.parse_args()
 
@@ -54,8 +54,8 @@ def write_img(path, img):
 
 def img_to_bw(img):
     def pixel_to_bw(pixel_val):
-        red, blue, green = pixel_val[0], pixel_val[1], pixel_val[2]
-        return int((red_c2b_ratio * red) + (blue_c2b_ratio * blue) + (green_c2b_ratio * green))
+        blue, green, red = pixel_val[0], pixel_val[1], pixel_val[2]
+        return int((blue_c2b_ratio * blue) + (green_c2b_ratio * green) + (red_c2b_ratio * red))
 
     gray_img = np.ndarray([img.shape[0], img.shape[1]], dtype=int)
     for i in range(gray_img.shape[0]):
@@ -65,22 +65,23 @@ def img_to_bw(img):
 
 def img_to_col(img):
     def pixel_to_col(pixel_val):
-        red = pixel_val * red_b2c_ratio
-        blue = pixel_val * blue_b2c_ratio
-        green = pixel_val * green_b2c_ratio
-        return [red, blue, green]
+        blue = int(pixel_val * blue_b2c_ratio)
+        green = int(pixel_val * green_b2c_ratio)
+        red = int(pixel_val * red_b2c_ratio)
+        return (blue, green, red)
 
     color_img = np.ndarray([img.shape[0], img.shape[1], 3], dtype=int)
     for i in range(color_img.shape[0]):
         for j in range(color_img.shape[1]):
-            color_img[i][j] = pixel_to_col(img[i][j])
+            cur_pixel = int((int(img[i][j][0]) + int(img[i][j][1]) + int(img[i][j][2])) / 3)
+            color_img[i][j] = pixel_to_col(cur_pixel)
     return color_img
 
 def main():
     print('Welcome to Pygmentize!')
     bwImage = read_img(flags.img_path)
     if bwImage.any(): #verify img is in numpy array
-        new_img = img_to_bw(bwImage)
+        new_img = img_to_col(bwImage)
         write_img(flags.out_path, new_img)
         print("Success!")
 
