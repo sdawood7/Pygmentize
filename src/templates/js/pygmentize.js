1c17bbdba719181cd1210259b110631d49b1685c
Oceana <oceana1129@gmail.com>
2021-06-29 22:22:21 -0400

    rudimentary way of adding uploaded img to the doc

--- src/templates/js/pygmentize.js
+++ src/templates/js/pygmentize.js
@@ -1,14 +1,25 @@
+// function pygmentize(){
+
+//     var filename = $('input[name=filename]').val()
+
+//     $.ajax({
+//         // type: 'post',
+//         url: "../main.py",
+//         context: document.body,
+//         data:{ img_path: filename }
+//     }).done(function() {
+//      alert(filename);
+//      console.log('!!!!!!!!!!!!!!!!!!!!!!!');
+//     });
+// }
 function pygmentize(){
-
-    var filename = $('input[name=filename]').val()
-
-    $.ajax({
-        // type: 'post',
-        url: "../main.py",
-        context: document.body,
-        data:{ img_path: filename }
-    }).done(function() {
-     alert(filename);
-     console.log('!!!!!!!!!!!!!!!!!!!!!!!');
-    });
+    //document.querySelector('#input').value
+    //input[type=submit].value;
+    //creating a variable and grabbing the info/data from the myFile input
+    // let img = document.querySelector('#myFile').value;
+    // creating a variable called image that is just the file/img we want to replace
+    let image = document.getElementById('mainImg');
+    
+    image.src = URL.createObjectURL(event.target.files[0]);
+    alert('done')
 }
